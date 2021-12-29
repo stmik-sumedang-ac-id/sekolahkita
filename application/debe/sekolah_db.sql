c6ef70e89bb89de922e467c7f17071936e05bbf0
yysofiyan <yanyansofiyan89@outlook.com>
2021-12-29 20:48:22 +0700

    debe

---
+++ application/debe/sekolah_db.sql
@@ -0,0 +1,620 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Dec 29, 2021 at 03:26 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sekolah_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_agenda`
+--
+
+CREATE TABLE `tbl_agenda` (
+  `agenda_id` int(11) NOT NULL,
+  `agenda_nama` varchar(200) DEFAULT NULL,
+  `agenda_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `agenda_deskripsi` text,
+  `agenda_mulai` date DEFAULT NULL,
+  `agenda_selesai` date DEFAULT NULL,
+  `agenda_tempat` varchar(90) DEFAULT NULL,
+  `agenda_waktu` varchar(30) DEFAULT NULL,
+  `agenda_keterangan` varchar(200) DEFAULT NULL,
+  `agenda_author` varchar(60) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_album`
+--
+
+CREATE TABLE `tbl_album` (
+  `album_id` int(11) NOT NULL,
+  `album_nama` varchar(50) DEFAULT NULL,
+  `album_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `album_pengguna_id` int(11) DEFAULT NULL,
+  `album_author` varchar(60) DEFAULT NULL,
+  `album_count` int(11) DEFAULT '0',
+  `album_cover` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_files`
+--
+
+CREATE TABLE `tbl_files` (
+  `file_id` int(11) NOT NULL,
+  `file_judul` varchar(120) DEFAULT NULL,
+  `file_deskripsi` text,
+  `file_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `file_oleh` varchar(60) DEFAULT NULL,
+  `file_download` int(11) DEFAULT '0',
+  `file_data` varchar(120) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_galeri`
+--
+
+CREATE TABLE `tbl_galeri` (
+  `galeri_id` int(11) NOT NULL,
+  `galeri_judul` varchar(60) DEFAULT NULL,
+  `galeri_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `galeri_gambar` varchar(40) DEFAULT NULL,
+  `galeri_album_id` int(11) DEFAULT NULL,
+  `galeri_pengguna_id` int(11) DEFAULT NULL,
+  `galeri_author` varchar(60) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_guru`
+--
+
+CREATE TABLE `tbl_guru` (
+  `guru_id` int(11) NOT NULL,
+  `guru_nip` varchar(30) DEFAULT NULL,
+  `guru_nama` varchar(70) DEFAULT NULL,
+  `guru_jenkel` varchar(2) DEFAULT NULL,
+  `guru_tmp_lahir` varchar(80) DEFAULT NULL,
+  `guru_tgl_lahir` varchar(80) DEFAULT NULL,
+  `guru_mapel` varchar(120) DEFAULT NULL,
+  `guru_photo` varchar(40) DEFAULT NULL,
+  `guru_tgl_input` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_inbox`
+--
+
+CREATE TABLE `tbl_inbox` (
+  `inbox_id` int(11) NOT NULL,
+  `inbox_nama` varchar(40) DEFAULT NULL,
+  `inbox_email` varchar(60) DEFAULT NULL,
+  `inbox_kontak` varchar(20) DEFAULT NULL,
+  `inbox_pesan` text,
+  `inbox_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `inbox_status` int(11) DEFAULT '1' COMMENT '1=Belum dilihat, 0=Telah dilihat'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_kategori`
+--
+
+CREATE TABLE `tbl_kategori` (
+  `kategori_id` int(11) NOT NULL,
+  `kategori_nama` varchar(30) DEFAULT NULL,
+  `kategori_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_kategori`
+--
+
+INSERT INTO `tbl_kategori` (`kategori_id`, `kategori_nama`, `kategori_tanggal`) VALUES
+(2, 'Politik', '2016-09-06 05:50:01'),
+(3, 'Sains', '2016-09-06 05:59:39'),
+(5, 'Penelitian', '2016-09-06 06:19:26'),
+(6, 'Prestasi', '2016-09-07 02:51:09'),
+(13, 'Olah Raga', '2017-01-13 13:20:31');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_kelas`
+--
+
+CREATE TABLE `tbl_kelas` (
+  `kelas_id` int(11) NOT NULL,
+  `kelas_nama` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_kelas`
+--
+
+INSERT INTO `tbl_kelas` (`kelas_id`, `kelas_nama`) VALUES
+(1, 'Kelas X.1'),
+(2, 'Kelas X.2'),
+(3, 'Kelas X.3'),
+(4, 'Kelas X.4'),
+(5, 'Kelas X.5'),
+(6, 'Kelas X.6'),
+(7, 'Kelas X.7'),
+(8, 'Kelas XI IPA.1'),
+(9, 'Kelas XI IPA.2'),
+(10, 'Kelas XI IPA.3'),
+(11, 'Kelas XI IPA.4'),
+(12, 'Kelas XI IPA.5'),
+(13, 'Kelas XI IPA.6'),
+(14, 'Kelas XI IPA.7'),
+(15, 'Kelas XI IPS.1'),
+(16, 'Kelas XI IPS.2'),
+(17, 'Kelas XI IPS.3'),
+(18, 'Kelas XI IPS.4'),
+(19, 'Kelas XI IPS.5'),
+(20, 'Kelas XI IPS.6'),
+(21, 'Kelas XI IPS.7');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_komentar`
+--
+
+CREATE TABLE `tbl_komentar` (
+  `komentar_id` int(11) NOT NULL,
+  `komentar_nama` varchar(30) DEFAULT NULL,
+  `komentar_email` varchar(50) DEFAULT NULL,
+  `komentar_isi` varchar(120) DEFAULT NULL,
+  `komentar_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `komentar_status` varchar(2) DEFAULT NULL,
+  `komentar_tulisan_id` int(11) DEFAULT NULL,
+  `komentar_parent` int(11) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_log_aktivitas`
+--
+
+CREATE TABLE `tbl_log_aktivitas` (
+  `log_id` int(11) NOT NULL,
+  `log_nama` text,
+  `log_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `log_ip` varchar(20) DEFAULT NULL,
+  `log_pengguna_id` int(11) DEFAULT NULL,
+  `log_icon` blob,
+  `log_jenis_icon` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_mapel`
+--
+
+CREATE TABLE `tbl_mapel` (
+  `id_mapel` int(11) NOT NULL,
+  `nama_mapel` varchar(100) NOT NULL,
+  `keterangan_mapel` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_mapel`
+--
+
+INSERT INTO `tbl_mapel` (`id_mapel`, `nama_mapel`, `keterangan_mapel`) VALUES
+(5, 'TIK', 'Teknologi Informasi dan Komunikasi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_pengguna`
+--
+
+CREATE TABLE `tbl_pengguna` (
+  `pengguna_id` int(11) NOT NULL,
+  `pengguna_nama` varchar(50) DEFAULT NULL,
+  `pengguna_moto` varchar(100) DEFAULT NULL,
+  `pengguna_jenkel` varchar(2) DEFAULT NULL,
+  `pengguna_username` varchar(30) DEFAULT NULL,
+  `pengguna_password` varchar(35) DEFAULT NULL,
+  `pengguna_tentang` text,
+  `pengguna_email` varchar(50) DEFAULT NULL,
+  `pengguna_nohp` varchar(20) DEFAULT NULL,
+  `pengguna_facebook` varchar(35) DEFAULT NULL,
+  `pengguna_twitter` varchar(35) DEFAULT NULL,
+  `pengguna_linkdin` varchar(35) DEFAULT NULL,
+  `pengguna_google_plus` varchar(35) DEFAULT NULL,
+  `pengguna_status` int(2) DEFAULT '1',
+  `pengguna_level` varchar(3) DEFAULT NULL,
+  `pengguna_register` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `pengguna_photo` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_pengguna`
+--
+
+INSERT INTO `tbl_pengguna` (`pengguna_id`, `pengguna_nama`, `pengguna_moto`, `pengguna_jenkel`, `pengguna_username`, `pengguna_password`, `pengguna_tentang`, `pengguna_email`, `pengguna_nohp`, `pengguna_facebook`, `pengguna_twitter`, `pengguna_linkdin`, `pengguna_google_plus`, `pengguna_status`, `pengguna_level`, `pengguna_register`, `pengguna_photo`) VALUES
+(1, 'Superadmin', 'HiddenProject', 'L', 'admin', 'd9da8170e8bc9f27b2d32a6c9a6c697d', 'HiddenProject', 'yysoftxx@hidden-project.xyz', '085696371900', '-', '-', '', '', 1, '1', '2021-12-28 06:07:55', 'a246139197bce7efc0daf9abe2305a42.jpg'),
+(2, 'Author Website', NULL, 'L', 'author', '02bd92faa38aaa6cc0ea75e59937a1ef', NULL, 'author@gmail.com', '085696371900', NULL, NULL, NULL, NULL, 1, '2', '2021-12-28 15:26:19', '86a2dc18d3ba16e5d2566b596d22d26c.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_pengumuman`
+--
+
+CREATE TABLE `tbl_pengumuman` (
+  `pengumuman_id` int(11) NOT NULL,
+  `pengumuman_judul` varchar(150) DEFAULT NULL,
+  `pengumuman_deskripsi` text,
+  `pengumuman_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `pengumuman_author` varchar(60) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_pengunjung`
+--
+
+CREATE TABLE `tbl_pengunjung` (
+  `pengunjung_id` int(11) NOT NULL,
+  `pengunjung_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `pengunjung_ip` varchar(40) DEFAULT NULL,
+  `pengunjung_perangkat` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_pengunjung`
+--
+
+INSERT INTO `tbl_pengunjung` (`pengunjung_id`, `pengunjung_tanggal`, `pengunjung_ip`, `pengunjung_perangkat`) VALUES
+(945, '2021-12-28 15:23:20', '::1', 'Chrome'),
+(946, '2021-12-28 17:02:31', '::1', 'Chrome');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_siswa`
+--
+
+CREATE TABLE `tbl_siswa` (
+  `siswa_id` int(11) NOT NULL,
+  `siswa_nis` varchar(20) DEFAULT NULL,
+  `siswa_nama` varchar(70) DEFAULT NULL,
+  `siswa_jenkel` varchar(2) DEFAULT NULL,
+  `siswa_kelas_id` int(11) DEFAULT NULL,
+  `siswa_photo` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_siswa`
+--
+
+INSERT INTO `tbl_siswa` (`siswa_id`, `siswa_nis`, `siswa_nama`, `siswa_jenkel`, `siswa_kelas_id`, `siswa_photo`) VALUES
+(11, '123089', 'Elizabeth ', 'P', 1, 'ec1232a08d650bc8c3197c9db95a7fc8.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_testimoni`
+--
+
+CREATE TABLE `tbl_testimoni` (
+  `testimoni_id` int(11) NOT NULL,
+  `testimoni_nama` varchar(30) DEFAULT NULL,
+  `testimoni_isi` varchar(120) DEFAULT NULL,
+  `testimoni_email` varchar(35) DEFAULT NULL,
+  `testimoni_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_tulisan`
+--
+
+CREATE TABLE `tbl_tulisan` (
+  `tulisan_id` int(11) NOT NULL,
+  `tulisan_judul` varchar(100) DEFAULT NULL,
+  `tulisan_isi` text,
+  `tulisan_tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `tulisan_kategori_id` int(11) DEFAULT NULL,
+  `tulisan_kategori_nama` varchar(30) DEFAULT NULL,
+  `tulisan_views` int(11) DEFAULT '0',
+  `tulisan_gambar` varchar(40) DEFAULT NULL,
+  `tulisan_pengguna_id` int(11) DEFAULT NULL,
+  `tulisan_author` varchar(40) DEFAULT NULL,
+  `tulisan_img_slider` int(2) NOT NULL DEFAULT '0',
+  `tulisan_slug` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_video`
+--
+
+CREATE TABLE `tbl_video` (
+  `id_video` int(11) NOT NULL,
+  `id_mapel` int(11) NOT NULL,
+  `kelas_id` int(11) NOT NULL,
+  `kode_video` varchar(100) NOT NULL,
+  `judul_video` varchar(150) NOT NULL,
+  `deskripsi_video` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_video`
+--
+
+INSERT INTO `tbl_video` (`id_video`, `id_mapel`, `kelas_id`, `kode_video`, `judul_video`, `deskripsi_video`) VALUES
+(8, 5, 8, 'AAu8bjj6-UI', 'Angular Tutorial for Beginners - Web Framework with Typescript Course', 'Learn the basics of Angular in this full course for beginners. Angular is a TypeScript-based free and open-source web application framework created by Google.');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_agenda`
+--
+ALTER TABLE `tbl_agenda`
+  ADD PRIMARY KEY (`agenda_id`);
+
+--
+-- Indexes for table `tbl_album`
+--
+ALTER TABLE `tbl_album`
+  ADD PRIMARY KEY (`album_id`),
+  ADD KEY `album_pengguna_id` (`album_pengguna_id`);
+
+--
+-- Indexes for table `tbl_files`
+--
+ALTER TABLE `tbl_files`
+  ADD PRIMARY KEY (`file_id`);
+
+--
+-- Indexes for table `tbl_galeri`
+--
+ALTER TABLE `tbl_galeri`
+  ADD PRIMARY KEY (`galeri_id`),
+  ADD KEY `galeri_album_id` (`galeri_album_id`),
+  ADD KEY `galeri_pengguna_id` (`galeri_pengguna_id`);
+
+--
+-- Indexes for table `tbl_guru`
+--
+ALTER TABLE `tbl_guru`
+  ADD PRIMARY KEY (`guru_id`);
+
+--
+-- Indexes for table `tbl_inbox`
+--
+ALTER TABLE `tbl_inbox`
+  ADD PRIMARY KEY (`inbox_id`);
+
+--
+-- Indexes for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  ADD PRIMARY KEY (`kategori_id`);
+
+--
+-- Indexes for table `tbl_kelas`
+--
+ALTER TABLE `tbl_kelas`
+  ADD PRIMARY KEY (`kelas_id`);
+
+--
+-- Indexes for table `tbl_komentar`
+--
+ALTER TABLE `tbl_komentar`
+  ADD PRIMARY KEY (`komentar_id`),
+  ADD KEY `komentar_tulisan_id` (`komentar_tulisan_id`);
+
+--
+-- Indexes for table `tbl_log_aktivitas`
+--
+ALTER TABLE `tbl_log_aktivitas`
+  ADD PRIMARY KEY (`log_id`),
+  ADD KEY `log_pengguna_id` (`log_pengguna_id`);
+
+--
+-- Indexes for table `tbl_mapel`
+--
+ALTER TABLE `tbl_mapel`
+  ADD PRIMARY KEY (`id_mapel`);
+
+--
+-- Indexes for table `tbl_pengguna`
+--
+ALTER TABLE `tbl_pengguna`
+  ADD PRIMARY KEY (`pengguna_id`);
+
+--
+-- Indexes for table `tbl_pengumuman`
+--
+ALTER TABLE `tbl_pengumuman`
+  ADD PRIMARY KEY (`pengumuman_id`);
+
+--
+-- Indexes for table `tbl_pengunjung`
+--
+ALTER TABLE `tbl_pengunjung`
+  ADD PRIMARY KEY (`pengunjung_id`);
+
+--
+-- Indexes for table `tbl_siswa`
+--
+ALTER TABLE `tbl_siswa`
+  ADD PRIMARY KEY (`siswa_id`);
+
+--
+-- Indexes for table `tbl_testimoni`
+--
+ALTER TABLE `tbl_testimoni`
+  ADD PRIMARY KEY (`testimoni_id`);
+
+--
+-- Indexes for table `tbl_tulisan`
+--
+ALTER TABLE `tbl_tulisan`
+  ADD PRIMARY KEY (`tulisan_id`),
+  ADD KEY `tulisan_kategori_id` (`tulisan_kategori_id`),
+  ADD KEY `tulisan_pengguna_id` (`tulisan_pengguna_id`);
+
+--
+-- Indexes for table `tbl_video`
+--
+ALTER TABLE `tbl_video`
+  ADD PRIMARY KEY (`id_video`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_agenda`
+--
+ALTER TABLE `tbl_agenda`
+  MODIFY `agenda_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_album`
+--
+ALTER TABLE `tbl_album`
+  MODIFY `album_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_files`
+--
+ALTER TABLE `tbl_files`
+  MODIFY `file_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_galeri`
+--
+ALTER TABLE `tbl_galeri`
+  MODIFY `galeri_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_guru`
+--
+ALTER TABLE `tbl_guru`
+  MODIFY `guru_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_inbox`
+--
+ALTER TABLE `tbl_inbox`
+  MODIFY `inbox_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  MODIFY `kategori_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `tbl_kelas`
+--
+ALTER TABLE `tbl_kelas`
+  MODIFY `kelas_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `tbl_komentar`
+--
+ALTER TABLE `tbl_komentar`
+  MODIFY `komentar_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_log_aktivitas`
+--
+ALTER TABLE `tbl_log_aktivitas`
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_mapel`
+--
+ALTER TABLE `tbl_mapel`
+  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tbl_pengguna`
+--
+ALTER TABLE `tbl_pengguna`
+  MODIFY `pengguna_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_pengumuman`
+--
+ALTER TABLE `tbl_pengumuman`
+  MODIFY `pengumuman_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_pengunjung`
+--
+ALTER TABLE `tbl_pengunjung`
+  MODIFY `pengunjung_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=947;
+
+--
+-- AUTO_INCREMENT for table `tbl_siswa`
+--
+ALTER TABLE `tbl_siswa`
+  MODIFY `siswa_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `tbl_testimoni`
+--
+ALTER TABLE `tbl_testimoni`
+  MODIFY `testimoni_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_tulisan`
+--
+ALTER TABLE `tbl_tulisan`
+  MODIFY `tulisan_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_video`
+--
+ALTER TABLE `tbl_video`
+  MODIFY `id_video` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
